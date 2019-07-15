d43b87e6667d491c3b80601c79e02cc2d8d2931e
andrevonah <andre2889@gmail.com>
2019-07-15 16:37:57 -0300

    estilizando tela inicial

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,104 @@
 import React from 'react';
+import { MdAddShoppingCart } from 'react-icons/md';
 
-// import { Container } from './styles';
+import { ProductList } from './styles';
 
 export default function Home() {
-  return <h1>Home</h1>;
+  return (
+    <ProductList>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-olympikus-contrast-masculino/02/D22-1776-002/D22-1776-002_detalhe2.jpg?resize=326:*"
+          alt="Tênis"
+        />
+        <strong>
+          Tênis olympikus com o título tão grande que quebra em um monte de
+          linhas
+        </strong>
+        <span>R$ 130,00</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-olympikus-contrast-masculino/02/D22-1776-002/D22-1776-002_detalhe2.jpg?resize=326:*"
+          alt="Tênis"
+        />
+        <strong>Tênis olympikus</strong>
+        <span>R$ 130,00</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-olympikus-contrast-masculino/02/D22-1776-002/D22-1776-002_detalhe2.jpg?resize=326:*"
+          alt="Tênis"
+        />
+        <strong>Tênis olympikus</strong>
+        <span>R$ 130,00</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-olympikus-contrast-masculino/02/D22-1776-002/D22-1776-002_detalhe2.jpg?resize=326:*"
+          alt="Tênis"
+        />
+        <strong>Tênis olympikus</strong>
+        <span>R$ 130,00</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-olympikus-contrast-masculino/02/D22-1776-002/D22-1776-002_detalhe2.jpg?resize=326:*"
+          alt="Tênis"
+        />
+        <strong>Tênis olympikus</strong>
+        <span>R$ 130,00</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-olympikus-contrast-masculino/02/D22-1776-002/D22-1776-002_detalhe2.jpg?resize=326:*"
+          alt="Tênis"
+        />
+        <strong>Tênis olympikus</strong>
+        <span>R$ 130,00</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+    </ProductList>
+  );
 }
