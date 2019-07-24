fae011498a1090007283e36486743421ed69342b
andrevonah <andre2889@gmail.com>
2019-07-23 22:21:25 -0300

    consultando estoque e exibindo toast se adicionar além dele

--- src/store/modules/cart/sagas.js
+++ src/store/modules/cart/sagas.js
@@ -1,4 +1,5 @@
 import { call, put, select, all, takeLatest } from 'redux-saga/effects';
+import { toast } from 'react-toastify';
 
 import api from '../../../services/api';
 import { formatPrice } from '../../../utils/format';
@@ -12,9 +13,19 @@ function* addToCart({ id }) {
     state.cart.find(p => p.id === id)
   );
 
-  if (productExists) {
-    const amount = productExists.amount + 1;
+  const stock = yield call(api.get, `/stock/${id}`);
 
+  const stockAmount = stock.data.amount;
+  const currentAmount = productExists ? productExists.amount : 0;
+
+  const amount = currentAmount + 1;
+
+  if (amount > stockAmount) {
+    toast.error('Quantidade indisponível');
+    return;
+  }
+
+  if (productExists) {
     yield put(updateAmount(id, amount));
   } else {
     const data = {
